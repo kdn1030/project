4dc517ad00ddfb5111034c3b841b45f671c425b8
dilectorpe <dilector@naver.com>
2019-04-05 20:41:05 +0900

    test for making folder and files
    test

---
+++ Chatting/MainActivity.java
@@ -0,0 +1,30 @@
+﻿package com.example.joongwon.chatting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+// test ssource ajldljalfal
+
+    // MainActivity 에서 Chat1 으로 이동할 수 있게 함
+    public void onClick(View v){
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.button1:
+                intent = new Intent(this,Chat1.class);
+// break;;;;;;
+                break;
+        }
+        startActivity(intent);
+    }
+
+}
